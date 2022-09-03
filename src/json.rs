0c2d9b6485a6af5ed123982829e675e3caf30f8b
Zalexanninev15 <blue.shark@disroot.org>
2022-09-03 21:20:34 +0300

    Version 1.3-beta 1.2 [unusable]
    Changes:
    - Added the function of downloading a file from the release
    - The process of introducing innovations has begun, so for now GRU is working with errors
    - Improved code documentation
    - Other changes

--- src/json.rs
+++ src/json.rs
@@ -1,12 +1,14 @@
 use isahc::ReadResponseExt;
 use serde_json::Value;
 
+// Main function of parser
 pub fn parse_data(repo: &str, search_words: &str) -> (String, String) {
     let json = get_text(&repo);
     let release_data = parse_text(&json, &search_words);
     release_data
 }
 
+// Parse json with release data
 fn parse_text(json: &str, word: &str) -> (String, String) {
     let release: Value = serde_json::from_str(json).expect("Error parsing JSON");
     let mut slob = String::from("app.zip");
@@ -18,6 +20,7 @@ fn parse_text(json: &str, word: &str) -> (String, String) {
     (release["tag_name"].to_string().replace("\"", ""), slob)
 }
 
+// Getting release information in json format
 fn get_text(repo: &str) -> String {
     let release_json = isahc::get(String::from(format!(
         "https://api.github.com/repos/{}/releases/latest",
