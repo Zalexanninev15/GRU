1f0b0716534bc99dc922e7f54389fbcaa249165b
Zalexanninev15 <blue.shark@disroot.org>
2022-09-12 21:34:45 +0300

    Version 1.3-rc2
    Changes:
    - Rewritten and corrected function to get information about current version from EXE file
    - Added support (excluding) for files that are just hash sums of real file from release assets
    - Added test deletion of temporary archives after retrieving a new version of the application from them
    - Improved message logic

--- src/get_version.rs
+++ src/get_version.rs
@@ -1,51 +1,47 @@
-use pelite::PeFile;
+use pelite::FileMap;
 
 // Checker for the versions
 pub fn is_new_version(new_version: &str, application_path: &str) -> i32 {
-    let file_map =
-        pelite::FileMap::open(application_path).expect("Cannot open the file specified!");
-    let image = pelite::PeFile::from_bytes(file_map.as_ref()).expect("File is not a PE image!");
-    let current_version = get_file_version(image).replace(", ", "");
-    if new_version.contains(&current_version) {
-        1
+    let mut current_version = parse_pe_version(application_path);
+    current_version = current_version.replace(", ", ".");
+    println!("\nCurrent version of app: {}", current_version);
+    if current_version.contains(&new_version) {
+        0
     } else {
         if current_version == "" {
             -1
         } else {
-            0
+            1
         }
     }
 }
 
-// fn get_bin_version(bin: PeFile<'_>) -> String {
-//     let resources = bin
-//         .resources()
-//         .expect("Error with extract the bin resources!");
-//     let version_info = resources
-//         .version_info()
-//         .expect("Error with extract the bin info!");
-//     let lang = version_info.translation()[0];
-//     let pe_version = version_info.value(lang, "FileVersion");
-//     (pe_version.unwrap())
-// }
-
 // Function for getting the file version from PE file
-fn get_file_version(bin: PeFile<'_>) -> String {
-    let resources = bin.resources().expect("Resources not found!");
-    let lang: Option<u16> = None;
-    let version_info = match lang {
-        Some(lang) => resources
-            .find_resource_ex(&[pelite::resources::Name::VERSION, 1.into(), lang.into()])
-            .and_then(|bytes| {
-                Ok(pelite::resources::version_info::VersionInfo::try_from(
-                    bytes,
-                )?)
-            }),
-        None => resources.version_info(),
+fn parse_pe_version(file_path: &str) -> String {
+    match FileMap::open(file_path) {
+        Err(_) => "".to_string(),
+        Ok(file_map) => {
+            let result = match pelite::PeFile::from_bytes(&file_map) {
+                Err(e) => Err(e),
+                Ok(f) => f.resources(),
+            };
+            match result {
+                Err(_) => "".to_string(),
+                Ok(resources) => match resources.version_info() {
+                    Err(_) => "".to_string(),
+                    Ok(version_info) => match version_info.fixed() {
+                        None => "".to_string(),
+                        Some(fixed_info) => {
+                            let version = fixed_info.dwFileVersion.to_string();
+                            if version.len() > 0 {
+                                version
+                            } else {
+                                fixed_info.dwProductVersion.to_string()
+                            }
+                        }
+                    },
+                },
+            }
+        }
     }
-    .expect("Version info not found!");
-    let lang = version_info.translation()[0];
-    // TODO Fix this error (?)
-    let file_version = format!("{:?}", version_info.value(lang, "ProductVersion"));
-    file_version
 }
