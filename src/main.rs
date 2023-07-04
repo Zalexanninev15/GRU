6d42d3962845c54cfd51c463e55d7c5bdee011d3
Zalexanninev15 <blue.shark@disroot.org>
2023-07-04 10:44:17 +0300

    Version 1.6 [DEV1]
    Changes:
    - Added the ability to download pre-releases (--pre) (in development)
    - Fixed a bug that appeared in version 1.5 and could cause a file unpacking/copying error
    - All dependencies have been updated to the latest versions
    - Unnecessary code sections have been removed
    - Other minor improvements have been made, a debugging mode (--debug) has been added in case of problems with the utility (only for developers, it will not give anything to ordinary users)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs::metadata;
 use std::path::Path;
 use std::process;
 
@@ -15,7 +16,8 @@ fn main() {
     const VERSION: &str = env!("CARGO_PKG_VERSION");
     let mut update_now = true;
     if arguments.len() >= 3 {
-        let arguments = arguments::parse(arguments)
+        let arguments = arguments
+            ::parse(arguments)
             .expect("Argument error! Check the arguments according to the \"help\" of the utility");
         let repo = arguments
             .get::<String>("repo")
@@ -29,17 +31,17 @@ fn main() {
         let real_app_path_bin = arguments
             .get::<String>("main")
             .unwrap_or(launcher_exe.to_string().parse().unwrap());
-
-        let is_zip = arguments.get::<bool>("extract").unwrap_or(true);
+        let is_extract = arguments.get::<bool>("extract").unwrap_or(true);
         let is_leave_folders = arguments.get::<bool>("leave").unwrap_or(false);
         let is_script_after = arguments.get::<bool>("script").unwrap_or(false);
+        let is_pre = arguments.get::<bool>("pre").unwrap_or(false);
         let silent_mode = arguments.get::<bool>("silent").unwrap_or(false);
-        winconsole::console::set_title("Github Release Updater")
+        let debug_mode = arguments.get::<bool>("debug").unwrap_or(false);
+
+        winconsole::console
+            ::set_title("Github Release Updater")
             .expect("Argument error! Check the arguments according to the \"help\" of the utility");
-        println!(
-            "Github Release Updater v{} by Zalexanninev15 <blue.shark@disroot.org>",
-            VERSION
-        );
+        println!("Github Release Updater v{} by Zalexanninev15 <blue.shark@disroot.org>", VERSION);
         if windows::is_app_elevated() {
             // Checking the Internet connection
             let ic = main_func::test_iconnection();
@@ -62,7 +64,7 @@ fn main() {
             }
 
             // Getting the new version release
-            let (v_list_version, mut v_list_asset) = json::parse_data(&repo, &part);
+            let (v_list_version, mut v_list_asset) = json::parse_data(&repo, &part, &is_pre);
 
             // Delete the hash-files from string
             v_list_asset = v_list_asset
@@ -70,6 +72,8 @@ fn main() {
                 .replace(".md5sum", "")
                 .replace(".md5", "")
                 .replace(".MD5", "")
+                .replace(".sha512", "")
+                .replace(".SHA512", "")
                 .replace(".sha256", "")
                 .replace(".SHA256", "")
                 .replace(".sha-1", "")
@@ -92,7 +96,9 @@ fn main() {
                 if version_status_code != 0 && create_only_version_file == false {
                     println!("\nNew version ({}) is available!", v_list_version);
                     if version_status_code == -1 {
-                        println!("\nHowever, it may be inaccurate, since. the original version was not correctly defined!")
+                        println!(
+                            "\nHowever, it may be inaccurate, since. the original version was not correctly defined!"
+                        );
                     }
                 } else {
                     update_now = false;
@@ -105,77 +111,100 @@ fn main() {
                 // Deleting unnecessary data
                 main_func::task_kill(&launcher_exe);
                 main_func::delete_file(&current_dir, &is_leave_folders);
-                
+
+                if debug_mode {
+                    println!("[Debug] State 1");
+                }
+
                 // Downloading the file
                 println!("Downloading...");
-                let result =
-                    downloader::download(&repo, &v_list_version, &v_list_asset, &current_dir);
-                if result.is_err() {
-                        println!("Failed to download!"); 
-                    }
+                downloader::download(&repo, &v_list_version, &v_list_asset, &current_dir);
+
+                if debug_mode {
+                    println!("[Debug] State 2");
                 }
-                else {
-                    // The updating process itself
-                    if first_launch {
-                        println!("Adding file(s)...");
-                    } else {
-                        println!("Updating...");
-                    }
-                    if is_zip {
-                        main_func::extracting(&current_dir);
-                    } else {
-                        let ue = main_func::updating(&current_dir, &launcher_exe);
-                        if ue.is_err() {
-                            println!("File replacement error!")
+
+                if let Ok(metadata) = metadata(String::from(format!("{}\\app.dat", &current_dir))) {
+                    if metadata.is_file() {
+                        if debug_mode {
+                            println!("[Debug] State 3");
                         }
-                    }
+                        // The updating process itself
+                        if first_launch {
+                            println!("Adding file(s)...");
+                        } else {
+                            println!("Updating...");
+                        }
+                        if is_extract {
+                            main_func::extracting(&current_dir);
+                        } else {
+                            let ue = main_func::updating(&current_dir, &launcher_exe);
+                            if ue.is_err() {
+                                println!("File replacement error!");
+                            }
+                        }
 
-                    // Delete the EXE file of the portable installer
-                    main_func::delete_file(&current_dir, &is_leave_folders);
-                    if is_script_after {
-                        println!("Running script.bat...");
-                        main_func::run_post_script(&current_dir);
-                    }
+                        // Delete the EXE file of the portable installer
+                        main_func::delete_file(&current_dir, &is_leave_folders);
 
-                    main_func::set_new_version(&v_list_version);
-                    if first_launch {
-                        println!("Download completed successfully!");
+                        if is_script_after {
+                            println!("Running script.bat...");
+                            main_func::run_post_script(&current_dir);
+                        }
+
+                        main_func::set_new_version(&v_list_version);
+                        if first_launch {
+                            println!("Download completed successfully!");
+                        } else {
+                            println!("Upgrade completed successfully!");
+                        }
                     } else {
-                        println!("Upgrade completed successfully!");
+                        println!("The file, for some reason, was not downloaded!");
                     }
                 }
-                if !silent_mode {
-                    press_btn_continue::wait("Press Enter to exit...").unwrap();
-                }
-                process::exit(0);
             } else {
-                press_btn_continue::wait("Administrator rights are required to run!").unwrap();
-                process::exit(1);
+                println!("The file, for some reason, was not downloaded!");
             }
+            if debug_mode {
+                println!("[Debug] State 4");
+            }
+            if !silent_mode {
+                press_btn_continue::wait("Press Enter to exit...").unwrap();
+            }
+            process::exit(0);
         } else {
+            press_btn_continue::wait("Administrator rights are required to run!").unwrap();
+            process::exit(1);
+        }
+    } else {
         const DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
-        println!("Github Release Updater
-Description: {}
-Version: v{}
-Developer: Zalexanninev15 <blue.shark@disroot.org>
-License: MIT License
-GitHub: https://github.com/Zalexanninev15/GRU\n
-USAGE:
-    gru.exe --repo <user/repository> --app <application.exe> --with <value for search>\n
-ARGUMENTS:
-    --repo <user/repository> — Set the repository of application
-    --app <application.exe> — Set the EXE of launcher/main application. The executable file must be located in a folder at a higher level, otherwise you need to set the '--main' argument with the correct path to the file
-    --with <value for search> — Set the part of name of asset in GitHub release for download, for example: \"win-amd64-portable.zip\"\n
-OPTIONAL:
-    --main <target> - Set the main part of the application, the path to the application located at the level above [Default value = value of the '--app' argument]
-    {{extract value}} → --extract or --no-extract — Set the type of file, extract archivers (flag) or copy EXE of launcher/main application [Default value: --extract]
-    {{leave value}} → --leave or --no-leave - Not delete or delete the unnecessary folders: $PLUGINSDIR, Other [Default value: --no-leave]
-    {{script value}} → --script or --no-script — Run the script (file \"script.bat\") after downloading the application or not [Default value: --no-script]
-    {{pause value}} → --silent or --no-silent — Hide the console after work or not [Default value: --no-silent]\n
-EXAMPLES:
-    gru.exe --repo gek64/GitHubDesktopPortable --app GitHubDesktopPortable.exe --with \"paf\" --main App\\GitHubDesktop\\GitHubDesktop.exe
-    gru.exe --repo flameshot-org/flameshot --app flameshot.exe --with \"win64.zip\" --script
-    gru.exe --repo jgraph/drawio-desktop --app app.exe --with \"-windows-32bit-no-installer.exe\" --no-extract\n", DESCRIPTION, VERSION);
+        println!(
+            "Github Release Updater
+    Description: {}
+    Version: v{}
+    Developer: Zalexanninev15 <blue.shark@disroot.org>
+    License: MIT License
+    GitHub: https://github.com/Zalexanninev15/GRU\n
+    USAGE:
+        gru.exe --repo <user/repository> --app <application.exe> --with <value for search>\n
+    ARGUMENTS:
+        --repo <user/repository> — Set the repository of application
+        --app <application.exe> — Set the EXE of launcher/main application. The executable file must be located in a folder at a higher level, otherwise you need to set the '--main' argument with the correct path to the file
+        --with <value for search> — Set the part of name of asset in GitHub release for download, for example: \"win-amd64-portable.zip\"\n
+    OPTIONAL:
+        --main <target> - Set the main part of the application, the path to the application located at the level above [Default value: value of the '--app' argument]
+        {{pre-release value}} → --pre or --no-pre - Use pre-releases when checking the latest version of an application  [Default value: --no-pre]
+        {{extract value}} → --extract or --no-extract — Set the type of file, extract archivers (flag) or copy EXE of launcher/main application [Default value: --extract]
+        {{leave value}} → --leave or --no-leave - Not delete or delete the unnecessary folders: $PLUGINSDIR, Other [Default value: --no-leave]
+        {{script value}} → --script or --no-script — Run the script (file \"script.bat\") after downloading the application or not [Default value: --no-script]
+        {{pause value}} → --silent or --no-silent — Hide the console after work or not [Default value: --no-silent]\n
+    EXAMPLES:
+        gru.exe --repo gek64/GitHubDesktopPortable --app GitHubDesktopPortable.exe --with \"paf\" --main App\\GitHubDesktop\\GitHubDesktop.exe
+        gru.exe --repo flameshot-org/flameshot --app flameshot.exe --with \"win64.zip\" --script
+        gru.exe --repo jgraph/drawio-desktop --app app.exe --with \"-windows-32bit-no-installer.exe\" --no-extract\n",
+            DESCRIPTION,
+            VERSION
+        );
     }
     // --rv <value> — Set the executable from which you want to take the current version of the application. If you don't know exactly where to take it from, put any value, e.g. \"0\"\n
     press_btn_continue::wait("Press Enter to exit...").unwrap();
