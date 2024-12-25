59d3be301d292a0aed1ee69da2eba6c3d1fec5b3
Max <blue.shark@disroot.org>
2024-12-25 13:07:41 +0300

    GRU v2.3-dev [2512]
    Problems:
    😢 Downloading with curl doesn't work!
    😢The built-in file uploader doesn't display the progress (bar) correctly, and it also doesn't display the file size!
    
    Changes:
    - "bn" → "gru"
    - Fixed problems if no executable or text file was found for "wget" and "curl" utilities, which caused critical errors in code
    - Updated the compilation instructions and compilation script for the Microsoft and Chocolatey dependency-free project, fixed minor issues
    - Rewritten and simplified the text of the help using ChatGPT (almost completely up-to-date old version of the help)
    - Improvements for "curl" and "wget" downloaders
    - Added support for "aria2c" as a downloader
    - Updated user-agent
    - Added an argument that prioritizes the configuration file instead of using the GRU argument properties. Support for wget (.wgetrc) and aria2c (aria2.conf)
    
    Build: Improved build script (build.ps1)

--- my_compiler.ps1
+++
@@ -1,9 +0,0 @@
-Remove-Item .\gru.exe -Force
-
-cargo build --release
-
-ResourceHacker -open .\target\release\gru.exe -save .\target\release\gru1.exe -action add -res .\ADMIN_MANIFEST.res -mask MANIFEST, -log CONSOLE
-Move-Item .\target\release\gru1.exe .\gru.exe -Force
-
-Write-Host Done!
-Write-Host File: gru.exe
