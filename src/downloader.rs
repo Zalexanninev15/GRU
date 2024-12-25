feaa04b63e080e9daaf43bbc05fe5fad5e0dbaa2
Max <blue.shark@disroot.org>
2024-12-25 13:07:41 +0300

    GRU v2.3-dev [2512]
    Problems:
    😢 Downloading with curl doesn't work!
    😢The built-in file uploader doesn't display the progress (bar) correctly, and it also doesn't display the file size!
    
    Changes:
    - "bn" → "gru"
    - Fixed problems if no executable or text file was found for "wget" and "curl" utilities, which caused critical errors in code
    - Updated the compilation instructions and compilation script for the Microsoft and Chocolatey dependency-free project, fixed minor issues
    - Rewritten and simplified the text of the help using ChatGPT (almost completely up-to-date old version of the help)
    - Improvements for "curl" and "wget" downloaders
    - Added support for "aria2c" as a downloader
    - Updated user-agent
    - Added an argument that prioritizes the configuration file instead of using the GRU argument properties. Support for wget (.wgetrc) and aria2c (aria2.conf)
    
    Build: Improved build script (build.ps1)

--- src/downloader.rs
+++ src/downloader.rs
@@ -20,7 +20,7 @@ pub fn download(
     details: &bool,
     mut downloader: &str,
     ua: &str,
-    use_cfg: &bool
+    mut use_cfg: &bool
 ) -> Result<(), Box<dyn std::error::Error>> {
     let current_dir = crate::main_func::current_dir();
     let file_name = String::from(format!("{}\\app.downloaded", current_dir));
@@ -37,7 +37,11 @@ pub fn download(
     }
 
     if downloader == "tcpu" {
-        execute_tcpu_download_script(main_func::read_downloadtool_config(), &asset, &current_dir);
+        let _ = execute_tcpu_download_script(
+            main_func::read_downloadtool_config(),
+            &asset,
+            &current_dir
+        );
     } else {
         match downloader {
             "curl" => {
@@ -54,9 +58,17 @@ pub fn download(
                     if Path::new(&String::from(format!("{}\\curl.exe", current_dir))).exists() {
                         command = Command::new(String::from(format!("{}\\curl.exe", current_dir)));
                     } else {
-                        downloader = "bn";
-                        let rt = tokio::runtime::Runtime::new().unwrap();
-                        let _ = rt.block_on(native(&current_dir, &file, &asset));
+                        if
+                            Path::new(
+                                &String::from(format!("C:\\Windows\\System32\\curl.exe"))
+                            ).exists()
+                        {
+                            command = Command::new("C:\\Windows\\System32\\curl.exe");
+                        } else {
+                            downloader = "gru";
+                            let rt = tokio::runtime::Runtime::new().unwrap();
+                            let _ = rt.block_on(native(&current_dir, &file, &asset));
+                        }
                     }
                 }
 
@@ -88,6 +100,8 @@ pub fn download(
                 }
             }
             "aria2c" => {
+                let mut cfg_path = String::new();
+
                 let mut command = Command::new("C:\\Windows\\System32\\aria2c.exe");
 
                 if Path::new(&String::from(format!("{}\\aria2c.txt", current_dir))).exists() {
@@ -96,89 +110,110 @@ pub fn download(
                     ).unwrap();
                     let mut contents = String::new();
                     file.read_to_string(&mut contents).unwrap();
-                    command = Command::new(contents.replace("\"", ""));
+                    let aria2_exe = contents.replace("\"", "");
+                    if *use_cfg {
+                        cfg_path = String::from(
+                            format!("{}", aria2_exe.replace("aria2c.exe", "aria2.conf"))
+                        );
+                    }
+                    command = Command::new(aria2_exe);
                 } else {
                     if Path::new(&String::from(format!("{}\\aria2c.exe", current_dir))).exists() {
+                        if *use_cfg {
+                            cfg_path = String::from(format!("{}\\aria2.conf", current_dir));
+                        }
                         command = Command::new(
                             String::from(format!("{}\\aria2c.exe", current_dir))
                         );
                     } else {
-                        downloader = "bn";
+                        downloader = "gru";
                         let rt = tokio::runtime::Runtime::new().unwrap();
                         let _ = rt.block_on(native(&current_dir, &file, &asset));
                     }
                 }
                 if downloader == "aria2c" {
-                    if *details {
+                    if *use_cfg {
                         command
                             .arg(asset)
+                            .arg(String::from(format!("--conf-path=\"{}\"", cfg_path)))
                             .arg(String::from(format!("--user-agent=\"{}\"", ua)))
-                            .arg("--console-log-level=error")
-                            .arg("--split=8")
-                            .arg("--max-connection-per-server=16")
-                            .arg("--min-split-size=4M")
-                            .arg("--piece-length=4M")
-                            .arg("--continue=false")
-                            .arg("--remote-time=true")
-                            .arg("--auto-file-renaming=false")
-                            .arg("--allow-overwrite=true")
-                            .arg("--connect-timeout=5")
-                            .arg("--lowest-speed-limit=5K")
-                            .arg("--max-tries=12")
-                            .arg("--max-file-not-found=12")
-                            .arg("--no-netrc=false")
-                            .arg("--timeout=60")
                             .arg("--check-certificate=false")
-                            .arg("--http-accept-gzip=true")
-                            .arg("--http-no-cache=true")
-                            .arg("--enable-http-keep-alive=true")
-                            .arg("--allow-piece-length-change=false")
-                            .arg("--conditional-get=true")
-                            .arg("--disable-ipv6=true")
-                            .arg("--disk-cache=4M")
-                            .arg("--download-result=hide")
-                            .arg("--file-allocation=falloc")
-                            .arg("--summary-interval=0")
                             .arg("-o")
                             .arg(file_name);
                     } else {
-                        command
-                            .arg(asset)
-                            .arg(String::from(format!("--user-agent=\"{}\"", ua)))
-                            .arg("--console-log-level=error")
-                            .arg("--split=8")
-                            .arg("--max-connection-per-server=16")
-                            .arg("--min-split-size=4M")
-                            .arg("--piece-length=4M")
-                            .arg("--continue=false")
-                            .arg("--remote-time=true")
-                            .arg("--auto-file-renaming=false")
-                            .arg("--allow-overwrite=true")
-                            .arg("--connect-timeout=5")
-                            .arg("--lowest-speed-limit=5K")
-                            .arg("--max-tries=12")
-                            .arg("--max-file-not-found=12")
-                            .arg("--no-netrc=false")
-                            .arg("--timeout=60")
-                            .arg("--check-certificate=false")
-                            .arg("--http-accept-gzip=true")
-                            .arg("--http-no-cache=true")
-                            .arg("--enable-http-keep-alive=true")
-                            .arg("--allow-piece-length-change=false")
-                            .arg("--conditional-get=true")
-                            .arg("--disable-ipv6=true")
-                            .arg("--disk-cache=4M")
-                            .arg("--download-result=hide")
-                            .arg("--file-allocation=falloc")
-                            .arg("--summary-interval=0")
-                            .arg("-o")
-                            .arg(file_name);
+                        if *details {
+                            command
+                                .arg(asset)
+                                .arg(String::from(format!("--user-agent=\"{}\"", ua)))
+                                .arg("--console-log-level=error")
+                                .arg("--split=8")
+                                .arg("--max-connection-per-server=16")
+                                .arg("--min-split-size=4M")
+                                .arg("--piece-length=4M")
+                                .arg("--continue=false")
+                                .arg("--remote-time=true")
+                                .arg("--auto-file-renaming=false")
+                                .arg("--allow-overwrite=true")
+                                .arg("--connect-timeout=5")
+                                .arg("--lowest-speed-limit=5K")
+                                .arg("--max-tries=12")
+                                .arg("--max-file-not-found=12")
+                                .arg("--no-netrc=false")
+                                .arg("--timeout=60")
+                                .arg("--check-certificate=false")
+                                .arg("--http-accept-gzip=true")
+                                .arg("--http-no-cache=true")
+                                .arg("--enable-http-keep-alive=true")
+                                .arg("--allow-piece-length-change=false")
+                                .arg("--conditional-get=true")
+                                .arg("--disable-ipv6=true")
+                                .arg("--disk-cache=4M")
+                                .arg("--download-result=hide")
+                                .arg("--file-allocation=falloc")
+                                .arg("--summary-interval=0")
+                                .arg("-o")
+                                .arg(file_name);
+                        } else {
+                            command
+                                .arg(asset)
+                                .arg(String::from(format!("--user-agent=\"{}\"", ua)))
+                                .arg("--console-log-level=error")
+                                .arg("--split=8")
+                                .arg("--max-connection-per-server=16")
+                                .arg("--min-split-size=4M")
+                                .arg("--piece-length=4M")
+                                .arg("--continue=false")
+                                .arg("--remote-time=true")
+                                .arg("--auto-file-renaming=false")
+                                .arg("--allow-overwrite=true")
+                                .arg("--connect-timeout=5")
+                                .arg("--lowest-speed-limit=5K")
+                                .arg("--max-tries=12")
+                                .arg("--max-file-not-found=12")
+                                .arg("--no-netrc=false")
+                                .arg("--timeout=60")
+                                .arg("--check-certificate=false")
+                                .arg("--http-accept-gzip=true")
+                                .arg("--http-no-cache=true")
+                                .arg("--enable-http-keep-alive=true")
+                                .arg("--allow-piece-length-change=false")
+                                .arg("--conditional-get=true")
+                                .arg("--disable-ipv6=true")
+                                .arg("--disk-cache=4M")
+                                .arg("--download-result=hide")
+                                .arg("--file-allocation=falloc")
+                                .arg("--summary-interval=0")
+                                .arg("-o")
+                                .arg(file_name);
+                        }
                     }
 
                     execute_command(command);
                 }
             }
             "wget" => {
+                let mut cfg_path = String::new();
+
                 let mut command = Command::new("C:\\Windows\\System32\\wget.exe");
 
                 if Path::new(&String::from(format!("{}\\wget.txt", current_dir))).exists() {
@@ -187,47 +222,67 @@ pub fn download(
                     ).unwrap();
                     let mut contents = String::new();
                     file.read_to_string(&mut contents).unwrap();
-                    command = Command::new(contents.replace("\"", ""));
+                    let wget_exe = contents.replace("\"", "");
+                    if *use_cfg {
+                        cfg_path = String::from(
+                            format!("{}", wget_exe.replace("wget.exe", ".wgetrc"))
+                        );
+                    }
+                    command = Command::new(wget_exe);
                 } else {
                     if Path::new(&String::from(format!("{}\\wget.exe", current_dir))).exists() {
+                        if *use_cfg {
+                            cfg_path = String::from(format!("{}\\.wgetrc", current_dir));
+                        }
                         command = Command::new(String::from(format!("{}\\wget.exe", current_dir)));
                     } else {
-                        downloader = "bn";
+                        downloader = "gru";
                         let rt = tokio::runtime::Runtime::new().unwrap();
                         let _ = rt.block_on(native(&current_dir, &file, &asset));
                     }
                 }
                 if downloader == "wget" {
-                    if *details {
+                    if *use_cfg {
                         command
-                            .arg(String::from(format!("-U=\"{}\"", ua)))
-                            .arg("--tries=2")
-                            .arg("--no-check-certificate")
-                            .arg("--cache=off")
-                            .arg("--header")
-                            .arg(String::from(format!("\"{}\"", ua)))
-                            .arg("-O")
-                            .arg(file_name)
-                            .arg(asset);
-                    } else {
-                        command
-                            .arg(String::from(format!("-U=\"{}\"", ua)))
-                            .arg("--tries=2")
-                            .arg("--no-check-certificate")
-                            .arg("--cache=off")
-                            .arg("--header")
-                            .arg(String::from(format!("\"{}\"", ua)))
-                            .arg("-q")
-                            .arg("-O")
-                            .arg(file_name)
                             .arg(asset)
-                            .arg("--show-progress");
+                            .arg(String::from(format!("--config=\"{}\"", cfg_path)))
+                            .arg(String::from(format!("-U=\"{}\"", ua)))
+                            .arg("--header")
+                            .arg(String::from(format!("\"{}\"", ua)))
+                            .arg("--check-certificate=false")
+                            .arg("-o")
+                            .arg(file_name);
+                    } else {
+                        if *details {
+                            command
+                                .arg(String::from(format!("-U=\"{}\"", ua)))
+                                .arg("--tries=2")
+                                .arg("--no-check-certificate")
+                                .arg("--cache=off")
+                                .arg("--header")
+                                .arg(String::from(format!("\"{}\"", ua)))
+                                .arg("-O")
+                                .arg(file_name)
+                                .arg(asset);
+                        } else {
+                            command
+                                .arg(String::from(format!("-U=\"{}\"", ua)))
+                                .arg("--tries=2")
+                                .arg("--no-check-certificate")
+                                .arg("--cache=off")
+                                .arg("--header")
+                                .arg(String::from(format!("\"{}\"", ua)))
+                                .arg("-q")
+                                .arg("-O")
+                                .arg(file_name)
+                                .arg(asset)
+                                .arg("--show-progress=on");
+                        }
                     }
-
                     execute_command(command);
                 }
             }
-            "bn" => {
+            "gru" => {
                 let rt = tokio::runtime::Runtime::new().unwrap();
                 let _ = rt.block_on(native(&current_dir, &file, &asset));
             }
