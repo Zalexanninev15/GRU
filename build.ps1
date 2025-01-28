b7f1dafc57175f2f20a6a3bc6a2c94f0a12309b9
Max <blue.shark@disroot.org>
2025-01-28 18:33:26 +0300

    Version 2.3.0 pre2-beta 2
    Changes:
    - Added the ability to work without administrator rights (unstable)
    - The tool has been renamed: "bn" to "gru"
    - Fixed problems if no executable or text file was found for "wget" and "curl" utilities, which caused critical errors in code.
    - Support for latest versions of wget and improved curl support.
    - Returned and updated code of debugging (--debug)
    - Rewritten and simplified the text of the help using ChatGPT (almost completely up-to-date old version of the help)
    - Improvements for "curl" and "wget" downloaders.
    - Updated user-agent.
    - A new argument has been added --wgetrc that prioritizes the configuration file instead of using the GRU argument properties. Support for wget (.wgetrc)
    - A new argument has been added --tcpud, which allows you to use the loader from Total Commander PowerUser (so far only "curl" is supported), the functionality may not be as stable as the other downloaders.
    - A new argument has been added --pre for pre-releases (if there are no stable ones or the pre-releases are newer than the stable ones)
    - A new argument has been added --before, which allows you to launch the prepare.bat before downloading the asset.
    - Fixed a bug with a missing version number if there are no stable releases.
    - Fixed a bug that could cause downloads to try to start even if no new version was found.
    - Optimized JSON parsing for releases, the used library for parsing has been updated to the latest version.
    - The curl-based loader built into the utility is used again by default.
    
    Build:
    - Improved build script (build.ps1)
    - Updated the compilation instructions for the Microsoft and Chocolatey dependency-free project, fixed minor issues.

--- build.ps1
+++ build.ps1
@@ -3,7 +3,7 @@ Remove-Item .\gru.exe -Force
 cargo clean
 cargo build --release
 
-ResourceHacker -open .\target\release\gru.exe -save .\gru.exe -action add -res .\ADMIN_MANIFEST.res -mask MANIFEST, -log CONSOLE
+ResourceHacker -open .\target\release\gru.exe -save .\gru_admin.exe -action add -res .\ADMIN_MANIFEST.res -mask MANIFEST, -log CONSOLE
 
 Write-Host Done!
 Write-Host File: gru.exe
